1df0a19641bc11f7b10f2dedb2a59a026535a44e
ryanxgraham <ryanxgraham@gmail.com>
2020-06-20 03:25:54 -0500

    added constants and helper functions

--- js/app.js
+++ js/app.js
@@ -17,14 +17,29 @@
  * Define Global Variables
  *
 */
+const sections = getElementsByTagName('section');
+const navContainer = getElementsByClassName('navbar__menu');
+const navList = getElementById('navbar__list');
+const navAnchor = getElementsByTagName('a');
 
 /**
  * End Global Variables
  * Start Helper Functions
  *
 */
+function insNavLink(navLinkName, sectionId) {
+    const htmlToAdd = <li><a href="#{sectionId}" class="menu__link">${navLinkName}</a></li>;
 
-
+    navList.insertAdjacentHTML("beforeend", htmlToAdd);
+}
+function topSectionInViewport(el) {
+    const rect = el.getBoundingClientRect();
+    return (
+        rect.top >= 0 &&
+        rect.top <=
+            o.4*(window.innerHeight || document.documentElement.clientHeight)
+    );
+}
 
 /**
  * End Helper Functions
